4e991fa073b50792e94034d8b0c9873e0376a1cf
kmorgport <72232365+kmorgport@users.noreply.github.com>
2023-05-15 17:59:15 -0500

    fix: add form and routes

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,15 +1,61 @@
 'use client';
 
 import { useState } from 'react'
+import { Session } from "next-auth";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
 
+interface ExtendedSession extends Session {
+  user: {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    id: string;
+  };
+}
+
 const CreatePrompt = () => {
-    
+    const router = useRouter();
+    const { data: session } = useSession()
+    const userId = (session as unknown as ExtendedSession)?.user.id;
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+      prompt: '',
+      tag: ''
+    })
+
+    const createPrompt = async (e:any) => {
+      e.preventDefault()
+      setSubmitting(true)
+      try{
+        const response = await fetch('api/prompt/new', 
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post. prompt,
+            userId: userId,
+            tag: post.tag
+          })
+        })
+        if(response.ok){
+          router.push('/')
+        }
+      }catch(error){
+
+      } finally {
+        setSubmitting(false)
+      }
+    }
   return (
-    <Form/>
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
   )
 }
 
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import Link from 'next/link'
+import { Dispatch, SetStateAction } from 'react'
 
-const Form = () => {
+interface PostType {
+  prompt: string;
+  tag: string;
+}
+
+interface FormProps {
+  type: string;
+  post: PostType;
+  setPost: Dispatch<SetStateAction<PostType>>;
+  submitting: boolean;
+  handleSubmit: (e: React.FormEvent) => void;
+}
+
+const Form: React.FC<FormProps> = ({type, post, setPost, submitting, handleSubmit,}) => {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span>{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share prompts with AI
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2x1 flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold test-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e)=>{setPost({ ...post,prompt:e.target.value })}}
+            placeholder="Write prompt here"
+            required
+            className="form_textarea"
+          >
+          </textarea>
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold test-base text-gray-700">
+            Tag{' '}
+            <span className="font-normal">(#product, #webidea)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e)=>{setPost({ ...post, tag:e.target.value })}}
+            placeholder="#tag"
+            required
+            className="form_input"
+          >
+          </input>
+        </label>
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>
+          <button
+          type="submit"
+          disabled={submitting}
+          className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting? `${type}...`:type}
+          </button>
+        </div>
+      </form>
+    </section>
   )
 }
 
